f73b03265fc1e7b80b9fcedd5e8f96bf487452a3
void <95859137+voiiddxx@users.noreply.github.com>
2024-02-02 12:25:52 +0530

    implemnted account updation feature using localstorage

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "../../styles/Home.css"
 const Home = () => {
+
+
+  const [email, setemail] = useState(null);
+  const [username, setusername] = useState(null);
+  const [password, setpassword] = useState(null);
+  
+
+  const handleSubmit = () => {
+    if(email && username && password){
+      // CLEARIGN THE USERNAME EMAIL AND PASSWORD
+      localStorage.removeItem("email");
+      localStorage.removeItem("username");
+      localStorage.removeItem("password");
+      // UPDATING THE EMAIL USERNAME AND PASSWORD
+
+      localStorage.setItem("email" , email);
+      localStorage.setItem("username" , username);
+      localStorage.setItem("password" , password);
+      alert("Your Account has been updated, You can use your new credentials for futher validation")
+    }
+    else{
+      alert("Please fill addtiniols fields")
+    }
+  }
   return (
-    <div>
-      <h1>This is home page</h1>
+    <div className='home'>
+      <div className="card">
+      <div className='heading' >
+        <h1>Update Your Accont Details</h1>
+        <br />
+      </div>
+     <form >
+     <input onChange={(e)=>{
+        setusername(e.target.value)
+     }} placeholder='your username'/>
+      <input onChange={(e)=>{
+        setemail(e.target.value)
+      }} placeholder='your email'/>
+      <input onChange={(e)=>{
+        setpassword(e.target.value)
+      }} placeholder='your password'/>
+      <div onClick={handleSubmit} className='button'>
+        <p>Update Now</p> </div>
+     </form>
+
+      </div>
     </div>
   )
 }
